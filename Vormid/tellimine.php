12951797d50ff4016c7c7164db58eb2493e04fbc
svensaal <Matiteab2019>
2020-01-12 22:49:04 +0200

    Update tellimine.php

--- Vormid/tellimine.php
+++ Vormid/tellimine.php
@@ -6,25 +6,21 @@
 	$toode1 = $_GET['t1'];
 	echo 'Toode 1: '.$toode1.'tk';
 //lisab vormist saadud andmed muutujasse
-$toode2 = $_GET['t2'];
-echo 'Toode 2: '.$toode2.'tk';
-$toode3 = $_GET['t3'];
-echo 'Toode 3: '.$toode3.'tk';
+//$toode2 = $_GET['t2'];
+//echo 'Toode 2: '.$toode2.'tk';
+//$toode3 = $_GET['t3'];
+//echo 'Toode 3: '.$toode3.'tk';
 
 function silindriRuumala($raadius, $korgus){
 	$pohjaPindala = pi() * $raadius * $raadius;
 	$ruumala = $pohjaPindala * $korgus;
 	return $ruumala;
 }
-echo '<pre>';
-print_r($_GET);
-echo '</pre>';
 
-$sr = $_get['sr'];
-$sk = $_get['sk'];
-echo
+$sr = $_GET['sr'];
+$sk = $_GET['sk'];
 
-$silindriRuumala = silindriRuumala($raadius,$korgus){
-echo 'Śilinder raadiusega' .$sr. 'ja kõrgusga ' .$sk. 'on 	
-}
+
+$silindriRuumala = silindriRuumala($sr, $sk);
+echo 'Silinder raadiusega '.$sr.' ja kõrgusega '.$sk.' on ruumalaga'.$silindriRuumala.'<br>';
 ?>
